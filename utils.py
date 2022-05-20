b27e0bc92a7ac7813d42702af85cbcfa14c6967d
Yuri Stepanov <trupakenator@gmail.com>
2022-05-20 23:13:33 +0300

    '2 commit'

--- utils.py
+++ utils.py
@@ -1,21 +1,24 @@
 import json
-from pprint import pprint
+
 
 from config.config import PATH
 
 
 def load_candidates_from_json(path=PATH):
+    """Загружаем файл .json"""
     with open(path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
 
 def get_all_candidates():
+    """Получаем список словарей из загруженного файла"""
     data = load_candidates_from_json()
     return data
 
 
 def get_candidate_pk(pk):
+    """Возвращаем кандидата по его id"""
     candidates = get_all_candidates()
     for candidate in candidates:
         if candidate['id'] == pk:
@@ -23,6 +26,7 @@ def get_candidate_pk(pk):
 
 
 def get_candidates_by_name(name):
+    """Возвращает кандидата по его имени"""
     candidates = get_all_candidates()
     candidates_name = []
     for candidate in candidates:
@@ -32,12 +36,10 @@ def get_candidates_by_name(name):
 
 
 def get_candidates_by_skill(skill):
+    """Возвращает кандидатов по навыку"""
     candidates = get_all_candidates()
     candidate_skill = []
     for candidate in candidates:
         if skill.lower() in candidate['skills']:
             candidate_skill.append(candidate)
     return candidate_skill
-
-
-# pprint(get_all_candidates())
